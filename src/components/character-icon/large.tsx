61c5c1c7e9aa12c834106fdc7c9656920fabc439
Djimo van Berlo <djimovanberlo@gmail.com>
2022-07-16 23:18:00 +0200

    quick rename

--- src/components/character-icon/large.tsx
+++ src/components/character-icon/large.tsx
@@ -1,7 +1,7 @@
 import { Character } from 'interfaces/game/character'
 
 const CharacterIconLarge = ({ character }: { character: Character }) => {
-  return <div className='characterIcon characterIcon--large'>{character}</div>
+  return <div className='characterIcon--large'>{character}</div>
 }
 
 export default CharacterIconLarge
--- src/components/character-icon/small.tsx
+++ src/components/character-icon/small.tsx
@@ -1,7 +1,7 @@
 import { Character } from 'interfaces/game/character'
 
 const CharacterIconSmall = ({ character }: { character: Character }) => {
-  return <div className='characterIcon characterIcon--small'>{character}</div>
+  return <div className='characterIcon--small'>{character}</div>
 }
 
 export default CharacterIconSmall
